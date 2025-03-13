3b4b3dab2d1fdc47fd1b57f855e8717f990b1163
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2025-03-13 14:47:02 +0100

    Added some useful tricks using ffmpeg

--- ffplay_test.sh
+++ ffplay_test.sh
@@ -1,4 +1,4 @@
-ffplay -f video4linux2 -video_size 1280x720 -i /dev/video0
+ffplay -f video4linux2 -video_size 1280x720 -i /dev/video2
 # equivalente: ffplay -f video4linux2 /dev/video0
 
 # To see available formats for devices:
@@ -6,3 +6,10 @@ ffplay -f video4linux2 -video_size 1280x720 -i /dev/video0
 
 # To see al available devices:
 # v4l2-ctl --list-devices
+
+
+# get raw data and convert
+# ffmpeg -f rawvideo -video_size 1920x1080 -pix_fmt yuyv422 -i out out.jpg
+
+# Everything in a single line:
+# gcc capture0.c -Wall && ./a.out -f -d /dev/video0 -o | ffmpeg -f rawvideo -video_size 1920x1080 -pix_fmt yuyv422 -i /dev/stdin out.jpg
