d9b958c91ff22414bb3c370bee8607661920c7b7
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2025-03-22 15:05:19 +0100

    Some tricks to send video to/from network with ffmpeg

--- ffplay_test.sh
+++ ffplay_test.sh
@@ -2,14 +2,20 @@ ffplay -f video4linux2 -video_size 1280x720 -i /dev/video2
 # equivalente: ffplay -f video4linux2 /dev/video0
 
 # To see available formats for devices:
-# ffmpeg -f v4l2 -list_formats all -i /dev/video0
+ffmpeg -f v4l2 -list_formats all -i /dev/video0
 
 # To see al available devices:
-# v4l2-ctl --list-devices
+v4l2-ctl --list-devices
 
 
 # get raw data and convert
-# ffmpeg -f rawvideo -video_size 1920x1080 -pix_fmt yuyv422 -i out out.jpg
+ffmpeg -f rawvideo -video_size 1920x1080 -pix_fmt yuyv422 -i out out.jpg
 
 # Everything in a single line:
 # gcc capture0.c -Wall && ./a.out -f -d /dev/video0 -o | ffmpeg -f rawvideo -video_size 1920x1080 -pix_fmt yuyv422 -i /dev/stdin out.jpg
+
+
+# send to network on port 48550
+ffmpeg -i /dev/video0 -framerate 15 -vcodec libx264 -preset ultrafast -tune zerolatency -maxrate 750k -bufsize 3000k -f mpegts udp://localhost:48550
+# receive from network port 48550
+ffplay -i "udp://localhost:48550"
