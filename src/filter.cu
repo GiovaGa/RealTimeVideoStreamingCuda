e467036ef5454d88045778fa2d439f61b3f86bd6
GiovaGa <48856010+GiovaGa@users.noreply.github.com>
2025-04-11 09:58:50 +0200

    Fixing overflow in filter.cu | rewritten function, untested on CUDA

--- src/filter.cu
+++ src/filter.cu
@@ -8,7 +8,7 @@
 #include <stdint.h>
 #include "filter.h"
 
-static const int32_t dimx = 11, dimy = dimx; 
+static const size_t dimx = 11, dimy = dimx; 
 
 #define minInt(a,b) (((a)<(b))?(a):(b))
 #define maxInt(a,b) (((a)<(b))?(b):(a))
@@ -17,60 +17,55 @@ static const int32_t dimx = 11, dimy = dimx;
 
 #ifdef __NVCC__
 __global__ void cuda_box_blur(const uint8_t __restrict__ *src, uint8_t __restrict__ *dest, const size_t width, const size_t height){
-    // const int index = threadIdx.x; const int stride = blockDim.x;
-    size_t i = blockIdx.x*blockDim.x + threadIdx.x;
-    if(i > height) return;
+    const int index = threadIdx.x; const int stride = blockDim.x;
+    for(size_t i =  threadIdx.x;i < height;i += blockDim.x){
 #else
 void box_blur(const uint8_t *restrict src, uint8_t *restrict dest, const size_t width, const size_t height){
-    for(int32_t i = 0;i < height;++i){
+    for(size_t i = 0;i < height;++i){
 #endif
-        const int32_t i1 = maxInt(i-((dimy)/2),0),
+        const size_t i1 = maxInt(i-((int)(dimy)/2),0),
                       i2 = minInt(i+((dimy+1)/2),height);
-        int32_t cnt = 0;
+        size_t j1 = 0, j2 = minInt(((dimx+1)/2),width);
         int32_t tmp[3] = {0,0,0};
-        int32_t j1 = 0,
-                j2 = ((dimx-1)/2);
+        // int32_t cnt = 0;
 
-        for(size_t jj = j1;jj < j2;++jj){
+        for(int jj = j1;jj < j2;++jj){
             for(size_t ii = i1;ii < i2;++ii){
                 for(size_t ch = 0; ch < 3;++ch){
                     tmp[ch] += src[index(ii,jj,ch)];
                 }
-                ++cnt;
+                // ++cnt;
             }
         }
-
         for(int32_t j = 0;j < width;++j){
-            j1 = j-((dimx)/2), j2 = j+((dimx+1)/2);
+            // assert(cnt > 0);
+            // assert(cnt == (i2-i1)*(j2-j1));
 
-            for(size_t ii = i1;ii < i2;++ii){
-                if(j2 < width){
-                    for(size_t ch = 0; ch < 3;++ch){
-                        tmp[ch] += src[index(ii,j2,ch)];
-                    }
-                    ++cnt;
-                }
-                if(j1 > 0){
-                    for(size_t ch = 0; ch < 3;++ch){
-                        tmp[ch] -= src[index(ii,j1-1,ch)];
-                    }
-                    --cnt;
-                }
-            }
+            const int32_t cnt = (i2-i1)*(j2-j1);
 
-            const int32_t jj1 = maxInt(j1,0),
-                          jj2 = minInt(j2,width);
-
-            assert(cnt > 0);
-            fprintf(stderr,"%d == (%d-%d)*(%d-%d)\n",cnt,i2,i1,jj2,jj1);
-            assert(cnt == (i2-i1)*(jj2-jj1));
-
+#ifdef DEBUG
+            // fprintf(stderr,"%d,%d -> ",i,j); fprintf(stderr,"%d == (%d-%d)*(%d-%d)\n",cnt,i2,i1,j2,j1);
             assert(tmp[0] >= 0);
             assert(tmp[0] <= 256*cnt);
-            cnt = (i2-i1)*(jj2-jj1);
-
+#endif
             for(size_t ch = 0; ch < 3;++ch){
                 dest[index(i,j,ch)] = (uint8_t)((tmp[ch])/cnt);
+            }
+            if(j2 < width){
+                for(size_t ii = i1;ii < i2;++ii){
+                    for(size_t ch = 0; ch < 3;++ch){
+                        tmp[ch] += src[index(ii,j2,ch)];
+                    } // ++cnt;
+                }
+                ++j2;
+            }
+            if(j2-j1 >= dimx){
+                for(size_t ii = i1;ii < i2;++ii){
+                    for(size_t ch = 0; ch < 3;++ch){
+                        tmp[ch] -= src[index(ii,j1,ch)];
+                    } // --cnt;
+                }
+                ++j1;
             }
         }
 #ifndef __NVCC__
